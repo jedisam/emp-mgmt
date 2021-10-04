3a3490dc8c5b587073bd297272ca65c65a23bd82
Yididya Samuel <yidisam18@gmail.com>
2021-10-04 09:37:57 +0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from "express"
+import mongoose from 'mongoose'
+import cors from "cors"
+import 'dotenv/config'
+
+const app = express();
+
+// Bodyparser middleware
+app.use(express.json());
+
+// enable cors
+app.use(cors());
+
+// Connect to Datase
+const uri: string = `${process.env.db}`;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log(`Succsessfully connected to DB`);
+});
+
+// Different routes
+app.get('/', (req, res) => {
+  res.send('Haloo');
+});
+
+
+const PORT: number = 7000;
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
+
+module.exports = app;
