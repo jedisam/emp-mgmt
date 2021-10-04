2ba253c4e1c63c38757d18758e597f40407558d3
Yididya Samuel <yidisam18@gmail.com>
2021-10-04 10:05:05 +0300

    adds employee model

---
+++ src/models/employee.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+const employeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name of the employee required!'],
+  },
+  dob: {
+    type: Date,
+    required: [true, 'Date of birth of the employee is required!'],
+  },
+  gender: {
+    type: String,
+    required: [true, 'Gender of the employee is required!'],
+  },
+  salary: {
+    type: Number,
+    required: [true, 'Salary of the employee is required'],
+  },
+  createdAt: {
+    type: String,
+    default: Date.now
+  }
+});
+
+const Employee = mongoose.model('Employee', employeeSchema);
+export default Employee;
